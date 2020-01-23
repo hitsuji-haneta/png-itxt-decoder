54468702495f474d89ef6c2cb5c3f2b5b3a96c15
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2020-01-23 11:27:48 +0900

    Run process wirh an arg

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 const pngitxt = require('png-itxt');
 
 console.log('start');
-
-fs.createReadStream('image/sample.png').pipe(
-  pngitxt.get(function(err, data) {
-    if (!err && data) {
-      console.log(data.keyword, ':', data.value);
-    }
-  })
-);
+if (process.argv.length >= 3) {
+  fs.createReadStream(process.argv[2]).pipe(
+    pngitxt.get(function(err, data) {
+      if (!err && data) {
+        console.log(data.keyword, ':', data.value);
+      }
+    })
+  );
+} else {
+  console.error('Set the image path to 3rd arg');
+}
